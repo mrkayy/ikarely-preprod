54040f842593a24e7b7a49489d39d1d699ac0384
olukayode joseph <olukayode_joseph@solutioninflux.com>
2021-04-23 16:09:00 +0100

    pulled last changes

--- src/shared/InputBox.js
+++ src/shared/InputBox.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-function InputBox({ value, label, name, onchange, err, hasError, type }) {
-  return (
-    <div className="input__box">
-      <label htmlFor={name}>{label}</label>
-      <input type={type} name={name} value={value[name]} onChange={onchange} />
-      <label htmlFor="password" className="has_error">
-        {hasError(name) ? err[name]?.error && err[name].message : null}
-      </label>
-=======
 import React, {useState, useContext} from "react";
 import { GlobalContext } from "../stores/GlobalLayer";
 import './InputBox.css'
@@ -25,7 +13,6 @@ function  InputBox({label, name, type })  {
       <label htmlFor={name} className="has__error">
         {errors[name]}
          </label>
->>>>>>> 33be542 (dasdads)
     </div>
   );
 }
