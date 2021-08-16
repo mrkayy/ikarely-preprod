5194e01e39068c162113155fe5cd448f7ab5647f
Olukayode Joseph <olukayode_joseph@solutioninflux.com>
2021-08-16 22:39:13 +0100

    pre-launch house cleaning

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,47 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { Grid, Paper, Container, Box, Divider } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+// import { Grid, Paper, Container, Box, Divider } from "@material-ui/core";
+// import { makeStyles } from "@material-ui/core/styles";
 
 import jwt_decode from "jwt-decode";
 import WebStorage from "../../shared/LocalStorage";
 
-import Menubar from "../../components/Clients/Menubar";
 import "../../components/Clients/style.css";
 
-import CMedicalHistory from "./MedicalHistory";
-import CMedicalProfile from "./MedicalProfile";
-import CAppointments from "./Appointments";
-import CDashboard from "./Dashboard";
-import CPayments from "./Payments";
-import CSettings from "./Settings";
-import CServices from "./ServiceRequests";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    marginTop: 87,
-    marginBottom: 30,
-    // background: '#e5e5e5',
-  },
-
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-
-  page: {
-    marginTop: 10,
-  },
-
-  main: {
-    // background: '#d4d4d4',
-    // padding: '0px 20px 20px 20px',
-  },
-}));
 
 const ClientProfile = () => {
-  const classes = useStyles();
   const history = useHistory();
 
   const token = WebStorage.get("user_token");
@@ -55,99 +23,11 @@ const ClientProfile = () => {
     return window.scrollTo(0, 0);
   };
 
-  const pgHistory = history.location.pathname;
   // allows app to display full dashboard
-  const currPage =
-    history.location.pathname === "/profile/dashboard" ? true : false;
-
-  const [pagepath, setPage] = useState({
-    path: [
-      { title: "Dashboard", path: "/dashboard" },
-      { title: "Medical Profile", path: "/medicals" },
-      { title: "Medical History", path: "/medical-history" },
-      { title: "Service Requests", path: "/service-requests" },
-      { title: "My Appointments", path: "/appointments" },
-      { title: "Payments", path: "/payments" },
-      { title: "Settings", path: "/settings" },
-      { title: "Support", path: "/support" },
-    ],
-    page: "/profile/dashboard",
-  });
-
-  useEffect(() => {
-    setPage((state) => ({ ...state, page: pgHistory }));
-    return () => {
-      // cleanup;
-    };
-  }, [pgHistory]);
-
-  const switchPage = (pg) => {
-    if (pg === pgHistory) {
-      return;
-    } else {
-      setPage((state) => ({ ...state, page: pg }));
-      history.push(pg);
-    }
-  };
 
   return (
     <>
-      <Container fixed className={classes.root}>
-        <Grid container spacing={2} direction="row">
-          <Grid item md={2}>
-            <Menubar pageparam={pagepath} switchPage={switchPage} />
-          </Grid>
-          {/* display main contents */}
-          <Grid item xs={12} md={currPage ? 10 : 7}>
-            {/* layout for a secondary navigation panel is optional /*}
-            {/*<Container fixed className={classes}>
-              <CNavBar /> 
-            </Container>
-              */}
-
-            <Container fixed className={classes.main}>
-              {
-                {
-                  "/dashboard": <CDashboard username={user} />,
-                  "/medicals": <CMedicalProfile username={user} />,
-                  "/medical-history": (
-                    <CMedicalHistory username={user.full_name} />
-                  ),
-                  "/service-requests": <CServices username={user} />,
-                  "/appointments": (
-                    <CAppointments username={user.full_name} />
-                  ),
-                  "/payments": <CPayments username={user.full_name} />,
-                  "/settings": <CSettings username={user.full_name} />,
-                  "/support": <Box>Support</Box>,
-                }[pagepath.page]
-              }
-            </Container>
-          </Grid>
-
-          {currPage ? (
-            <></>
-          ) : (
-            <>
-              <Grid item xs={12} md={3}>
-                <Grid container direction="column">
-                  <Grid item>
-                    <Paper className={classes.paper} elevation={1}>
-                      <div className="notification__title">
-                        <p>Notification Menu</p>
-                      </div>
-                      <Divider />
-                      <div className="notification__menu">
-                        <p>No Notification Here</p>
-                      </div>
-                    </Paper>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </>
-          )}
-        </Grid>
-      </Container>
+    <h1>Hello every one!</h1>
     </>
   );
 };
